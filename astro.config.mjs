caa202b965ab6b34cb0630b401e3740bf6e25d83
thinkerpot <thinkerpot@gmail.com>
2025-06-01 18:59:46 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blahg.netlify.app/",
+	site: "https://finance.educentrals.com/",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
