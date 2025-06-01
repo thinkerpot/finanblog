4a187a61fec57359b4838bf90f959d0ac24683ee
thinkerpot <thinkerpot@gmail.com>
2025-06-01 19:06:30 +0800

    Update config.js Optimasi

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Finance by EduCentrals";
+export const SITE_DESCRIPTION = "Tips Keuangan, Bisnis, dan Investasi Cerdas untuk Generasi Melek Cuan.";
