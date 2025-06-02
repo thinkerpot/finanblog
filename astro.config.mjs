69e52a781145393c8145a27aa8213996e6d5f238
thinkerpot <thinkerpot@gmail.com>
2025-06-02 17:20:56 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://finance.educentrals.com/",
+	site: "https://finance.educentrals.com",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
